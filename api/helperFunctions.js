0ac7795fc9a5ee34dac0058aae265c6793f6352d
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-21 14:59:36 -0700

    created helper functions and endpoints

---
+++ api/helperFunctions.js
@@ -0,0 +1,32 @@
+const db = require('./data/dbConfig')
+
+module.exports = {
+    getAll,
+    postPhysicist,
+    deletePhysicist,
+    getById
+}
+
+function getAll(){
+    return db('physicists')
+}
+
+function postPhysicist(physicist){
+    return db('physicists')
+        .insert(physicist, 'id')
+        .then(id => {
+            return getById(id[0])
+        })
+}
+
+function deletePhysicist(id){
+    return db('physicists')
+        .where({ id })
+        .del()
+}
+
+function getById(id){
+    return db('physicists')
+        .where({ id })
+        .first()
+}
